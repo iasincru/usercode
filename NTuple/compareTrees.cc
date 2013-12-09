3fdba8b4c74239d8fe1d0579ffae7f28d2efd8b2
Ivan <ivan.asin.cruz@cern.ch>
2013-12-09 11:57:43 +0100

    Add macro

---
+++ NTuple/compareTrees.cc
@@ -0,0 +1,44 @@
+//////////////////////////////////////////////////////////////////
+////
+////     root -l -b -q compareTrees.cc++
+////
+//////////////////////////////////////////////////////////////////
+
+#include <TFile.h>
+#include <TLegend.h>
+#include <TH1.h>
+#include <TTree.h>
+#include <TStyle.h>
+#include <TCanvas.h>
+
+#include "iostream"
+
+void compareTrees(){
+
+    TFile *_file0 = TFile::Open("/data/group/top/DiffXS/2013_11_04_TAG_N002_45M/ttbarsignalplustau.root");
+    TFile *_file1 = TFile::Open("/data/group/top/DiffXS/2013_11_04_TAG_N002_MadSpin_FullStats62M/ttbarsignalplustau.root");
+
+    TTree *tree0 = (TTree*)_file0->Get("writeNTuple/NTuple");
+    TTree *tree1 = (TTree*)_file1->Get("writeNTuple/NTuple");
+
+    gStyle->SetOptStat(0);
+
+    TH1D* h0 = new TH1D("h0", "h0", 60, -0.5, 59.5);   h0->SetLineColor(kRed);
+    TH1D* h1 = new TH1D("h1", "h1", 60, -0.5, 59.5);   h1->SetLineColor(kBlue);
+
+    tree0->Draw("@allGenJets.size()>>h0", "", "norm");
+    tree1->Draw("@allGenJets.size()>>h1", "", "norm,same");
+
+
+    TLegend *leg = new TLegend(0.75, 0.75, 0.9, 0.9);
+    leg->AddEntry(h0, "Madgraph", "l");
+    leg->AddEntry(h1, "MadSpin", "l");
+
+    TCanvas *c1 = new TCanvas();
+    h0->DrawNormalized();
+    h1->DrawNormalized("same");
+    leg->Draw("same");
+
+    c1->Print("compareTree.eps");
+
+}
